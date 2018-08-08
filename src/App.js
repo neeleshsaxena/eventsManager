77f1dce3734ec9247eb702968a0ad50339e2f334
Neelesh <saxena.ne@husky.neu.edu>
2018-08-08 00:03:44 -0400

    Initial version of Events Manager App

--- src/App.js
+++ src/App.js
@@ -1,8 +1,21 @@
 import React, { Component } from 'react';
+import {connect} from 'react-redux';
+import {bindActionCreators} from 'redux';
+import PropTypes from 'prop-types';
+
 import logo from './logo.svg';
 import './App.css';
+import * as fetchActions from './actions/fetchEvents';
+import EventList from './components/eventList';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+
+    // This binding is necessary to make `this` work in the callback
+    this.handleClick = this.handleClick.bind(this);
+  }
+
   render() {
     return (
       <div className="App">
@@ -11,11 +24,30 @@ class App extends Component {
           <h1 className="App-title">Welcome to React</h1>
         </header>
         <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
+          To check out events in Washington DC, Maryland area, <button onClick={this.handleClick} >Click Me! </button>
         </p>
+        <EventList/>
       </div>
     );
   }
+  handleClick() {
+    this.props.fetchActions.fetchEvents();
+  };
 }
 
-export default App;
+App.propTypes = {
+  fetchActions: PropTypes.object,
+  events: PropTypes.array
+};
+
+
+function mapDispatchToProps(dispatch) {
+  return {
+    fetchActions: bindActionCreators(fetchActions, dispatch)
+  };
+}
+
+export default connect(
+  undefined,
+  mapDispatchToProps
+)(App);
